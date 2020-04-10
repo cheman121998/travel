4b35957bb95568260beb34bbf958d1e883215f49
Man Man <chethiminhman121998@gmail.com>
2020-04-10 16:57:53 +0700

    Update file UI web Travel

--- travel/App_Start/BundleConfig.cs
+++ travel/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace travel
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.min.css",
                       "~/Content/site.css"));
         }
     }
